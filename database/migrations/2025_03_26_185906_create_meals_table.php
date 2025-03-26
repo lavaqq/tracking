a783be1be11d857a846c6ab40b64682d0f9e0d1c
lavaqq <mlava@tuta.io>
2025-03-26 21:23:43 +0100

    chore: remove useless col

--- database/migrations/2025_03_26_185906_create_meals_table.php
+++ database/migrations/2025_03_26_185906_create_meals_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('type'); // enum: Enums\MealType.php
-            $table->text('description')->nullable();
             $table->text('recipe')->nullable();
             $table->timestamps();
         });
